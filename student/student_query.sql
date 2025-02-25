cb31e8c0277508e93f1513ddc3eb5af2afda8327
Ullas789 <Ullas78999@gmail.com>
2025-02-25 23:41:25 +0530

    get all marks of a student given usn for the latest seem

--- student/student_query.sql
+++ student/student_query.sql
@@ -6,3 +6,11 @@ q0 = Student.objects.values_list('Current_sem',flat=True).distinct().first()
 
 -- list all student belonging to sem 1
 q1=Student.objects.filter(Current_sem='1').values_list('Name',flat=True).first()
+
+
+
+-- get all marks of a student given usn for the latest seem
+
+cur_sem=Student.objects.filter(Usn='vv005').values_list('Current_sem',flat=True).first()
+q3=Marks.objects.filter(Student_Id__Usn='vv005').get(Sem_Id__Sem=cur_sem)
+print(q3.Marks)
