19e9a90e978cdb25ee17940e16c5650975fb6d2e
Ullas789 <Ullas78999@gmail.com>
2025-02-25 23:40:58 +0530

    list all student belonging to sem 1

--- student/student_query.sql
+++ student/student_query.sql
@@ -1,2 +1,8 @@
 -- Get all distinct sem of student
 q0 = Student.objects.values_list('Current_sem',flat=True).distinct().first()
+
+
+
+
+-- list all student belonging to sem 1
+q1=Student.objects.filter(Current_sem='1').values_list('Name',flat=True).first()
