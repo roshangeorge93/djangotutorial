3b49bb5c58c788b5e7be19aac6bb25cbc0d23c91
Ullas789 <Ullas78999@gmail.com>
2025-02-25 21:58:53 +0530

    get contact details by giving name

--- employees/query.sql
+++ employees/query.sql
@@ -2,3 +2,8 @@
 q0=Contact.objects.select_related('Emp__department').all()
 for q in q0:
     print(f"{q.Emp.Name} {q.Number} {q.Emp.department.department_id}")
+
+--get contact details by giving name
+q1=Contact.objects.filter(Emp__Name='e1')
+for q in q1:
+    print(q.Number)
