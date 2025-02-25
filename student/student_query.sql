d1e0a3b7ad1b4258805085374df8ab6e8c6025b2
Ullas789 <Ullas78999@gmail.com>
2025-02-25 23:41:59 +0530

    get all marks of a student given usn for a particular subject

--- student/student_query.sql
+++ student/student_query.sql
@@ -14,3 +14,19 @@ q1=Student.objects.filter(Current_sem='1').values_list('Name',flat=True).first()
 cur_sem=Student.objects.filter(Usn='vv005').values_list('Current_sem',flat=True).first()
 q3=Marks.objects.filter(Student_Id__Usn='vv005').get(Sem_Id__Sem=cur_sem)
 print(q3.Marks)
+
+-- get all marks of a student given usn for a particular subject
+sub=Subject.objects.filter(Sub_name='DS').values_list('Sub_Id',flat=True).first()
+q4=Marks.objects.filter(Student_Id__Usn='vv003').get(Sub_Id=sub)
+print(q4.Marks)
+
+
+
+ 
+-- get the total percentage of marks for a student in each sem
+
+
+
+
+
+-- get total percentage of student of all sem
