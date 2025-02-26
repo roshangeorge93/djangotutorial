34a94d5fdfd93134c744a63c206eb665d3ee407e
Ullas789 <Ullas78999@gmail.com>
2025-02-26 20:29:39 +0530

    list all products And filter based on cat_id

--- products/product_query.py
+++ products/product_query.py
@@ -4,3 +4,6 @@ from products.models import Product,Brand,Catageory
 q0=Product.objects.values_list('P_name',flat=True)
 print(q0)
 
+# list all products And filter based on cat_id
+q1=Product.objects.filter(Cat_id='20').values('P_name','Cat_id')
+print(q1)
