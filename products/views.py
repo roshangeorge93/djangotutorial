9def1adb67009017fad863256cff5e793c45874e
Ullas789 <Ullas78999@gmail.com>
2025-02-26 23:23:00 +0530

    logic to display the product details

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from products.models import Product,Brand,Catageory
+from django.db.models import F
 # Create your views here.
 
 
@@ -7,5 +8,33 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def products(request):
-    return HttpResponse("Hello, world. You're at the product index.")
+
+
+# def products(request,employee_id):
+#     Employee_details=Product.objects.filter(id=employee_id).values()
+    
+    
+#     context = {
+#         "Employee_details": Employee_details,
+#     }
+    
+#     return render(request, "Employee_details.html", context)
+
+def display(request,product_P_Id):
+    product_details=Product.objects.filter(P_Id=product_P_Id).values()
+    
+    
+    context = {
+        "product_details": product_details,
+    }
+    
+    return render(request, "product_details.html", context)
+
+def index(request):
+    product_list = Product.objects.all()
+    
+    context = {
+        "product_list": product_list,
+    }
+    
+    return render(request, "products.html", context)
