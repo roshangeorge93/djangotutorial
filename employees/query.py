a729ca05b4e339d9c9a69cc8c5be88563090e7ab
Ullas789 <Ullas78999@gmail.com>
2025-02-26 15:44:47 +0530

    ist all emplyoee and contact number

---
+++ employees/query.py
@@ -0,0 +1,7 @@
+from employees.models import Employee,Contact,Department
+
+
+# ---list all emplyoee and contact number
+q0=Contact.objects.select_related('Emp__department').all()
+for q in q0:
+    print(f"{q.Emp.Name} {q.Number} {q.Emp.department.department_id}")
