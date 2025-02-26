78e46cc24b7b84bc5001d2b89f77c148316354cb
Ullas789 <Ullas78999@gmail.com>
2025-02-26 15:45:59 +0530

    location city_state_country

--- employees/query.py
+++ employees/query.py
@@ -16,3 +16,9 @@ for q in q1:
 q2=Employee.objects.filter(contact__Number='1234567891')
 for q in q2:
     print(q.Name)
+
+
+# --location city_state_country
+q3=Employee.objects.select_related('department__l').all()
+for q in q3:
+    print(f"{q.Name} { q.department.department_id} {q.department.department_name} {q.department.l.l_city}_{q.department.l.l_state}_{q.department.l.l_country}")
